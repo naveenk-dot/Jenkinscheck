8edb31ce6d20f63376cd1147ec3ee676a8f41601
naveen <naveenkundgolece16@gmail.com>
2025-02-06 17:03:05 +0530

    <commit>

---
+++ Jen01/src/test/java/JenkinTest/Jen01/dummyTest.java
@@ -0,0 +1,15 @@
+package JenkinTest.Jen01;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class dummyTest {
+	
+	  @Test
+	    public void shouldAnswerWithTrue() {
+	        assertTrue(true);
+	        System.out.println("Testing123");
+	    }
+
+}
