5540d3d99c844be7f58c4a5b94b247c9a9d1e8b4
naveen <naveenkundgolece16@gmail.com>
2025-02-06 19:44:29 +0530

    <Committed>

--- Jen01/src/test/java/JenkinTest/Jen01/OpenBrowser.java
+++ Jen01/src/test/java/JenkinTest/Jen01/OpenBrowser.java
@@ -1,9 +1,14 @@
 package JenkinTest.Jen01;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.logging.Level;
+
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.logging.LogType;
+import org.openqa.selenium.logging.LoggingPreferences;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -13,10 +18,16 @@ public class OpenBrowser {
 	  @Test
 	    public void openBRowser() {
 		  WebDriverManager.chromiumdriver().setup();
-			WebDriver driver=new ChromeDriver();
+		  ChromeOptions options = new ChromeOptions();
+	        options.addArguments("--headless"); // Run browser in headless mode
+	        LoggingPreferences logPrefs = new LoggingPreferences();
+	        logPrefs.enable(LogType.BROWSER, Level.ALL);
+	        options.setCapability("goog:loggingPrefs", logPrefs);
+	        WebDriver driver = new ChromeDriver(options);
+	        driver.get("https://www.youtube.com/watch?v=iYaTTkpQIbQ&t=1206s");
 			driver.get("https://www.youtube.com/watch?v=iYaTTkpQIbQ&t=1206s");
 			driver.manage().window().maximize();	
-			System.out.println("naveen1234567");
+			System.out.println("naveen12345jijij67");
 	    }
 
 }
